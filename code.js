9d49b6d48de886412b3a07760bc7ff1afd559cea
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-10-13 15:20:29 -0500

    unit tests for edge cases

--- code.js
+++ code.js
@@ -3,24 +3,44 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 
-function sayHello(x) {
-    let helloJane = "Hello, Jane!"
-    let helloAlex = "Hello, Alex!"
-    let helloPat = "Hello, Pat!"
-    let helloWorld = "Hello, World!"
+// TODO DONE!
 
-    if (x === "Jane") {
-        return helloJane
+function sayHello(x) {
+    if (typeof x !== "string" || x === "" || x === "5") {
+        return "Hello, World!"
     }
-    else if (x === "Alex") {
-        return helloAlex
-    }
-    else if (x === "Pat") {
-        return helloPat
-    }
-    else if (typeof x === "boolean" || x === "World") {
-        return helloWorld
+    else {
+        return `Hello, ${x}!`
     }
 }
 
+// TODO NOT DONE!
+
+function isFive(x) {
+    if (x === 5) {
+        return true
+    }
+    if (typeof x !== "number" || x !== 5) {
+        return false
+    }
+}
+
+//  Inside of tests.js, write a describe block for our new isFive function.
+//  As your first, failing test, write an it and an expect asserting that a function named isFive exists.
+//  Run the tests by refreshing report.html to show the red, failing test.
+//  Write just enough code inside of code.js to define an empty function for isFive.
+//  Now, refresh report.html to ensure that all tests are green.
+//  What other tests and implementation cycles should you do for isFive?
+//  Ensure that isFive returns a boolean no matter what the input
+//  Ensure that isFive returns true when passed 5
+//  What about if we pass in the string "5"? Do you want isFive to return true for that?
+//  If so, write the test, ensure that the test is failing, and then write the implementation
+//  Commit your work to git and push to GitHub before moving forward.
+
+
+
+
+// TODO NOT DONE!
+function sayGoodbye() {
+}
 
--- tests.js
+++ tests.js
@@ -1,47 +1,77 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+        expect(typeof helloWorld).toBe('function');
     });
     it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+        expect(typeof helloWorld()).toBe("string");
     });
     it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+        expect(helloWorld()).toBe("Hello, World!");
     });
     it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+describe(`sayHello`, function(){
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it(`should return "Hello, Jane!"`, function () {
+        expect(sayHello(`Jane`)).toBe(`Hello, Jane!`)
+    });
+    it(`should return "Hello, Alex!"`, function () {
+        expect(sayHello(`Alex`)).toBe(`Hello, Alex!`)
+    });
+    it(`should return "Hello, Pat!"`, function () {
+        expect(sayHello(`Pat`)).toBe(`Hello, Pat!`)
+    });
+    it(`undefined should return Hello, World!`, function () {
+        expect(sayHello()).toBe("Hello, World!")
+    });
+    it(`true should return Hello, World!`, function () {
+        expect(sayHello(true)).toBe("Hello, World!")
+    });
+    it(`false should return Hello, World!`, function () {
+        expect(sayHello(false)).toBe("Hello, World!")
+    });
+    it(`"5" should return Hello, World!`, function () {
+        expect(sayHello(`5`)).toBe("Hello, World!")
+    });
+    it(`null should return Hello, World!`, function () {
+        expect(sayHello(null)).toBe("Hello, World!")
+    });
+    it(`Empty Strings should return Hello, World!`, function () {
+        expect(sayHello(``)).toBe("Hello, World!")
+    });
+    it(`Arrays should return Hello, World!`, function () {
+        expect(sayHello([2, 3, 4])).toBe("Hello, World!")
+    });
+    it(`sayHello should return Hello, World!`, function () {
+        expect(sayHello(sayHello)).toBe("Hello, World!")
+    });
+
+});
+
+describe('isFive', function() {
+    it('should be a defined function', function () {
+        expect(typeof isFive).toBe('function');
+    });
+    it(`any input should return a boolean`, function () {
+        expect(isFive()).toBe( false)
+    });
+    it('5 should return true', function () {
+        expect(isFive(5)).toBe(true)
+    });
+    it('"5" should return false', function () {
+        expect(isFive("5")).toBe(false)
     });
 });
 
 
-describe('sayHello', function() {
-    it('should be a defined function', function () {
-        expect(typeof sayHello).toBe('function');
-    });
-    it('should be a string', function () {
-        expect(typeof sayHello()).toBe('string');
-    });
-    it("should return 'Hello, Jane!'", function () {
-        expect(sayHello("Jane")).toBe('Hello, Jane!');
-    });
-    it("should return 'Hello, Alex!'", function () {
-        expect(sayHello("Alex")).toBe('Hello, Alex!');
-    });
-    it("should return 'Hello, Pat!'", function () {
-        expect(sayHello("Pat")).toBe('Hello, Pat!');
-    });
-    it('should return "Hello, World!', function () {
-        expect(sayHello("World")).toBe("Hello, World!")
-    });
-    it('should return "Hello, World!" if true', function () {
-        expect(sayHello(true)).toBe("Hello, World!")
-    });
-    it('should return "Hello, World!" if false ', function () {
-        expect(sayHello(false)).toBe("Hello, World!")
-    });
-})
-
+// What about if we pass in the string "5"? Do you want isFive to return true for that?
+// If so, write the test, ensure that the test is failing, and then write the implementation
 
 
 
